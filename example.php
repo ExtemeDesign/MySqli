cee011c76a81f5af9eb37d5c82ad0b244a19168c
dragan88ar <dragan88_ar@hotmail.com>
2015-09-21 13:07:32 +0200

    example.php

---
+++ example.php
@@ -0,0 +1,19 @@
+<?php
+require_once("class.MySql.php");
+
+	$host       =   "localhost";
+    $username   =   "username";
+    $password   =   "sifra";
+    $baza       =   "dbname";
+
+$db = new SQL ($host, $username, $password, $baza);
+
+
+$zaDodavanje=array(
+    'id' => NULL,
+    'ime'=>'Petar',
+    'prezime'=>'Petrovic'
+);
+$db->dodaj("imebaze",$zaDodavanje);
+
+?>
